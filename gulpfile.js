08c4060d6d3f0c8432ca536217140c1f001ee573
Bob Steen <13983@ma-web.nl>
2015-06-15 14:46:30 +0200

    almost finished gulpInit

--- gulpfile.js
+++ gulpfile.js
@@ -10,5 +10,36 @@ var gulp = require('gulp');
     cache = require('gulp-cache'),
     del = require('del');
 
+var paths = {
+    scripts: ['angular/lib/*.js],
+    styles: 'angular/style/**/*.css],['angular/style/**/*.scss]
+};
 
-gulp.task('default',['watch','scripts','data']); //calling the above defined gulp tasks to run by default
+gulp.task('styles',function() {
+    return sass(path.styles), {style: 'expanded'})
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('angular/compressed/css'))
+    .pipe(rename({suffix:'.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('angular/compressed/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts',function(){
+    return gulp.src('angular/lib/**/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('angular/compressed/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('angular/compressed/js'))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+
+gulp.task('clean', function(cb) {
+    del(['angular/compressed/css', 'angular/compressed/js'], cb)
+    });
+
+gulp.task('default',['clean','styles','scripts']); //calling the above defined gulp tasks to run by default
