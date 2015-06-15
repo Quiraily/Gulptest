a09d1176a89d4e74d1ac771f04135ce8bf35bad3
Bob Steen <13983@ma-web.nl>
2015-06-15 14:13:09 +0200

    added package.json and gulp require's

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
-
-
-
+    sass = require('gulp-ruby-sass');
+    autoprefixer = require('gulp-autoprefixer');
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    del = require('del');
 
 
 gulp.task('default',['watch','scripts','data']); //calling the above defined gulp tasks to run by default
